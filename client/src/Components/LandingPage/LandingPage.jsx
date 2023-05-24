7f3be034ef2eb6842fbffaf0b343141a4edaaaea
ignaciocenni <ignaziocenni@gmail.com>
2023-05-24 00:19:50 -0300

    update

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -6,8 +6,8 @@ export default function LandingPage() {
 	return (
 		<div className={style.container}>
 			<div className={style.welcome}>
-				<h1 className={style.title}>Individual Project Countries</h1>
-				<h2 className={style.subtitle}>Created by Ignacio Cenni</h2>
+				<h1 className={style.title}>Proyecto Individual 'Countries'</h1>
+				<h2 className={style.subtitle}>Creado por Ignacio Cenni</h2>
 				<Link to='/home'>
 					<button className={style.boton}>Ingresar</button>
 				</Link>
